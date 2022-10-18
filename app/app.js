afed2f4a60f28e99e3b07344974fd11baf27db54
Summer S <snowykittin@gmail.com>
2022-10-18 14:27:41 -0400

    updates

--- app/app.js
+++ app/app.js
@@ -1,37 +1,56 @@
-const orders = [];
-let ordName;
-let ordDate;
-let ordDetail;
-let ordType;
-
 class NewOrder{
-    //Create constructor with values for name, date, type, and details
+    //Create an overall orders array
+    orders = {};
+
+
     //Use this class to then store values into the array, that then can be output to HTML
     orderPlaced(event){
         //Prevents refresh of the page on click
         event.preventDefault();
     
-        ordName = document.getElementById("orderName").value;
-        ordDate = document.getElementById("orderDate").value;
-        ordType = document.getElementById("orderTypes").value;
-        ordDetail = document.getElementById("orderDetails").value;
+        let ordName = document.getElementById("orderName").value;
+        let ordDate = document.getElementById("orderDate").value;
+        let ordType = document.getElementById("orderTypes").value;
+        let ordDetail = document.getElementById("orderDetails").value;
 
-        orders.push({
-            ordName,ordDate,ordType,ordDetail
-        });
+        //create an array item for each order, then send it to the overall array
+        const order = [ordName, ordDate, ordType, ordDetail];
+        orders.push(order);
        
-        DisplayOrder.displayData();
-    
+        //update display
+        DisplayOrder.displayData(orders);
+        //sort data
+        OrderData.dataSort(ordType);
     }
+
+    //order removed function
+    orderRemoved(event){
+        //prevent refresh on click
+        event.preventDefault;
+
+        //remove last order
+        orders.pop();
+
+        //update display
+        DisplayOrder.displayData(orders);
+    }
+
 };
 
 class DisplayOrder{
     //Class will use contents of X instance from newOrder to output it to the HTML. 
     //Class here will also be used to remove content from HTML
-    displayData(){
-        ordBox = document.getElementById("orders");
-
+    constructor(order){
+        this.order = order;
     }
+
+    //display the data based off of the orders array, which is updated each time a button is clicked
+    displayData(order){
+        ordBox = document.getElementById("orders");
+        ordBox.innerHTML = order;
+    }
+
+
 };
 
 class OrderData{
@@ -45,12 +64,14 @@ class OrderData{
     countD = 0;
 
     //data sorting to prepare for the graph
-    dataSort(order){
-        if(order == valA){
+    dataSort(orderType){
+        this.order = orderType;
+
+        if(orderType == "Volvo"){
             countA++;
-        }else if(order == valB){
+        }else if(order == "Saab"){
             countB++
-        }else if(order == valC){
+        }else if(order == "Opel"){
             countC++;
         }else{
             countD++;
@@ -60,19 +81,40 @@ class OrderData{
     }
 };
 
+//instantiate the initial class
+const OrderSystem = new NewOrder;
+
+//grab button IDs to begin executing the code
+let btnSubmit = document.getElementById("turnIn");
+let btnRemove = document.getElementById("removeOrder");
+
+btnSubmit.addEventListener("click", OrderSystem.orderPlaced);
+btnRemove.addEventListener("click", OrderSystem.orderRemoved);
+
 //overall onClick function for debugging
-function orderPlaced(event){
-    //Prevents refresh of the page on click
-    event.preventDefault();
+// const orders = [];
+// ordID = 0;
+// function orderPlaced(event){
+//     //Prevents refresh of the page on click
+//     event.preventDefault();
 
-    ordName = document.getElementById("orderName").value;
-    ordDate = document.getElementById("orderDate").value;
-    ordType = document.getElementById("orderTypes").value;
-    ordDetail = document.getElementById("orderDetails").value;
+//     ordName = document.getElementById("orderName").value;
+//     ordDate = document.getElementById("orderDate").value;
+//     ordType = document.getElementById("orderTypes").value;
+//     ordDetail = document.getElementById("orderDetails").value;
+//     ordID = ordID + 1;
 
-    orders.push({
-        ordName,ordDate,ordType,ordDetail
-    });
-    console.log(orders[0]);
+//     order = [ordID, ordName, ordDate, ordType, ordDetail, "</br>"];
+//     orders.push(order);
+//     console.log(orders);
 
-}
+//     ordBox = document.getElementById("orders");
+//         ordBox.innerHTML = orders + "</br>";
+
+// }
+
+// function removeOrder(event){
+//     event.preventDefault()
+
+    
+// }
